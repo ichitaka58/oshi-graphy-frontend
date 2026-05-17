4a1ef665c3c32e68b0739f58e10e183b14b9b90a
ichitaka58 <ichinosetakayuki92@gmail.com>
2026-05-17 21:06:45 +0900

    refactor: login/page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,31 +1,8 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import LoginForm from "./LoginForm";
 
 const Login = () => {
   return (
     <div className="min-h-screen max-w-4xl w-full mx-auto flex items-center justify-center p-4">
-      {/* <Card className="w-full max-w-sm">
-        <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input id="email" type="email" placeholder="oshigraphy@example.com" required />
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" required />
-              </div>
-            </div>
-          </form>
-        </CardContent>
-        <CardFooter>
-          <Button type="submit" className="w-full">Login</Button>
-        </CardFooter>
-      </Card> */}
       <LoginForm />
     </div>
   );
