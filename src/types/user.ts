eddcb5b57a69689108e758f3f6dc1e2c93dce4d1
ichitaka58 <ichinosetakayuki92@gmail.com>
2026-05-21 10:21:28 +0900

    feat: update types for comments and users

---
+++ src/types/user.ts
@@ -0,0 +1,12 @@
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+  icon_path: string;
+  profile: string;
+  is_admin: boolean;
+  email_verified_at: string;
+  created_at: string;
+  updated_at: string;
+  icon_url: string;
+};
