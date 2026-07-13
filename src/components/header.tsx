b50e9b0ac65a24429bd3581ffd96739b5869603c
ichitaka58 <ichinosetakayuki92@gmail.com>
2026-07-13 19:47:47 +0900

    feat: ヘッダーコンポーネントにユーザー名表示機能を追加

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,12 @@ const Header = async () => {
     <div className="h-12 bg-primary">
       <div className="flex items-center justify-between h-12 max-w-4xl mx-auto px-4">
         <img src="/oshi-graphy-text-logo.png" className="h-8" />
-        {user && <HeaderUserMenu user={user} />}
+        {user && (
+          <div className="flex items-center gap-2">
+            <p className="text-xs text-primary-foreground">{user.name}</p>
+            <HeaderUserMenu user={user} />
+          </div>
+        )}
       </div>
     </div>
   );
