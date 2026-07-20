99c1069cd129a6d4b0e9b2ed00fce26c0e2ec36a
ichitaka58 <ichinosetakayuki92@gmail.com>
2026-07-20 13:23:49 +0900

    feat: Notification型を新規作成し、通知データの構造を定義

---
+++ src/types/notification.ts
@@ -0,0 +1,19 @@
+export type Notification = {
+  id: string;
+  type: string;
+  notifiable_type: string;
+  notifiable_id: number;
+  data: {
+    type: "like" | "comment" | "reply" | "follow";
+    target?: "diary" | "comment";
+    dairy_id?: number;
+    comment_id?: number;
+    actor_user_id: number;
+    actor_name: string;
+    target_user_id?: number;
+    message: string;
+  }
+  read_at: string | null;
+  created_at: string;
+  updated_at: string;
+}
