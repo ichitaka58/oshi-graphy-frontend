de80576d38c72252477c80b7267849cc66ef2957
ichitaka58 <ichinosetakayuki92@gmail.com>
2026-08-05 11:06:04 +0900

    feat: ホームページのロゴ・キャッチコピーにフェードインアニメーションを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,34 +4,36 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center h-screen justify-center gap-4 mt-6 bg-zinc-50 font-sans dark:bg-black">
-      <div>
-        <Image
-          src="/oshi-graphy-logo.png"
-          alt="application-logo"
-          width={200}
-          height={200}
-          priority
-        />
-      </div>
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-gray-800 dark:text-gray-300 mb-2 sm:mb-4 tracking-wider text-lg sm:text-2xl">
-          推しグラフィー
-        </h1>
-        <p className="font-extrabold text-xl">
-          <span className="block text-center">おとなの</span>
-          <span className="block text-center">
-            <span className="text-lg text-[#F8DE6F] text-shadow-sm text-shadow-gray-400">
-              推し活
+      <div className="flex flex-col items-center gap-4 motion-safe:animate-fade-up">
+        <div>
+          <Image
+            src="/oshi-graphy-logo.png"
+            alt="application-logo"
+            width={200}
+            height={200}
+            priority
+          />
+        </div>
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h1 className="text-gray-800 dark:text-gray-300 mb-2 sm:mb-4 tracking-wider text-lg sm:text-2xl">
+            推しグラフィー
+          </h1>
+          <p className="font-extrabold text-xl">
+            <span className="block text-center">おとなの</span>
+            <span className="block text-center">
+              <span className="text-lg text-[#F8DE6F] text-shadow-sm text-shadow-gray-400">
+                推し活
+              </span>
+              <span>を記憶し</span>
             </span>
-            <span>を記憶し</span>
-          </span>
-          <span className="text-center">感動を共有しよう</span>
-        </p>
-        <p className="text-xs text-gray-800 dark:text-gray-300">
-          ライブ参戦・写真・メモをまとめて管理。
-          <br />
-          思い出を美しく残そう。
-        </p>
+            <span className="text-center">感動を共有しよう</span>
+          </p>
+          <p className="text-xs text-gray-800 dark:text-gray-300">
+            ライブ参戦・写真・メモをまとめて管理。
+            <br />
+            思い出を美しく残そう。
+          </p>
+        </div>
       </div>
       <div className="flex flex-col items-center gap-1 mt-4">
         <p className="text-sm">
